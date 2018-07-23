24b6e80258596f32d7d813f8ed8e22e4df823a04
Yusuke Morishita <yeemulator@gmail.com>
2018-07-23 10:40:48 +0900

    Changes to be committed: 	modified:   getFriendList.py

--- getFriendList.py
+++ getFriendList.py
@@ -1,13 +1,13 @@
 # -*- coding:utf-8 -*-
 
-import json, config #標準のjsonモジュールとconfig.pyの読み込み
-from requests_oauthlib import OAuth1Session #OAuthのライブラリの読み込み
+import json, config
+from requests_oauthlib import OAuth1Session
 
 CK = config.CONSUMER_KEY
 CS = config.CONSUMER_SECRET
 AT = config.ACCESS_TOKEN
 ATS = config.ACCESS_TOKEN_SECRET
-twitter = OAuth1Session(CK, CS, AT, ATS) #認証処理
+twitter = OAuth1Session(CK, CS, AT, ATS)
 
 url = "https://api.twitter.com/1.1/friends/list.json" #list of follower
 
@@ -17,12 +17,12 @@ while True:
     params ={'count' : count, 'cursor' : cursor}
     res = twitter.get(url, params = params)
 
-    if res.status_code == 200: #正常通信出来た場合
-        friends = json.loads(res.text) #レスポンスから取得
+    if res.status_code == 200: # success
+        friends = json.loads(res.text)
         for names in friends['users']:
             if names['friends_count'] >= 10000 :
                 print(names['name']+':'+names['screen_name']+':'+str(names['friends_count']))
-    else: #正常通信出来なかった場合
+    else:
         print("Failed: %d" % res.status_code)
         break
 
